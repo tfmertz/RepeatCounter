4c1976eeb27eb7048dc145a8d1f8ac7c3fb4c2b1
Tom Mertz <tom.mertz@verizon.net>
2015-03-13 11:01:12 -0700

    Add twig functionality

--- app/app.php
+++ app/app.php
@@ -4,11 +4,11 @@
     require_once __DIR__.'/../src/RepeatCounter.php';
 
     $app = new Silex\Application();
+    $app->register(new Silex\Provider\TwigServiceProvider(), array('twig.path' => __DIR__.'/../views'));
 
+    $app->get('/', function() use ($app) {
 
-    $app->get('/', function() {
-
-        return "HELLO";
+        return $app['twig']->render('form.twig');
 
     });
 
