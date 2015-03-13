2d234448ca102a6c4efbb138c025058633d30243
Tom Mertz <tom.mertz@verizon.net>
2015-03-13 11:20:01 -0700

    Add twig results page into routing

--- app/app.php
+++ app/app.php
@@ -4,11 +4,22 @@
     require_once __DIR__.'/../src/RepeatCounter.php';
 
     $app = new Silex\Application();
+
+    $app['debug'] = true;
+
     $app->register(new Silex\Provider\TwigServiceProvider(), array('twig.path' => __DIR__.'/../views'));
 
     $app->get('/', function() use ($app) {
 
         return $app['twig']->render('form.twig');
+
+    });
+
+    $app->post('/results', function() use ($app) {
+
+        
+
+        return $app['twig']->render('results.twig');
 
     });
 
