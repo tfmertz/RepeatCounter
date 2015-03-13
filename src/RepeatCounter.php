9d082ec7510988ee5554a072e76ce5825511e6c7
Tom Mertz <tom.mertz@verizon.net>
2015-03-13 10:16:43 -0700

    Pass the two matching letters test

--- src/RepeatCounter.php
+++ src/RepeatCounter.php
@@ -6,13 +6,18 @@
             //create a new array of each word from the sentence
             $list_of_words = explode(" ", $string_to_search);
 
+            //create a counter for the num of word matches
+            $count = 0;
+
             //loop through the list of words and find if there's a match
             foreach($list_of_words as $word)
             {
                 if($input_word == $word) {
-                    return 1;
+                    $count++;
                 }
             }
+
+            return $count;
         }
 
     }
