a449cae8ef4631c4b4d39683ffab130826823614
Tom Mertz <tom.mertz@verizon.net>
2015-03-13 14:07:48 -0700

    Pass punctuation split test

--- src/RepeatCounter.php
+++ src/RepeatCounter.php
@@ -31,11 +31,35 @@
             return $count;
         }
 
-        //NOT USED IN countRepeats FUNCTION AT ALL, purely used for twig output
-        //Used to split a string into an array with punctuation saved as elements
+        //NOT USED IN countRepeats FUNCTION AT ALL, PURELY USED FOR TWIG OUTPUT
+
+        //Split a string into an array with punctuation saved as elements
         //input: a string
         //output: an array of strings
         function splitPunctuation($input_string) {
+
+            //put the string into an array word by word including punctuation
+            $list_of_words = explode(" ", $input_string);
+
+            //make an empty array to push our words and punctuation to separately
+            $words_punctuation_list = array();
+
+            //loop through our words, some may be containing punctuation
+            foreach($list_of_words as $word) {
+
+                //check if the last letter of the word is punctuation
+                if(fnmatch("[^a-zA-Z]", substr($word, -1)))
+                {
+                    //push the word to our punctuation array
+                    array_push($words_punctuation_list, substr($word, 0, -1));
+                    //push the punctuation to our array
+                    array_push($words_punctuation_list, substr($word, -1));
+                } else {
+                    array_push($words_punctuation_list, $word);
+                }
+            }
+
+            return $words_punctuation_list;
 
         }
 
