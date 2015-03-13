46709fe7d1b696e86026ddcfe5109eac0b9346a8
Tom Mertz <tom.mertz@verizon.net>
2015-03-13 10:53:07 -0700

    Add working silex foundation

--- app/app.php
+++ app/app.php
@@ -3,4 +3,15 @@
     require_once __DIR__.'/../vendor/autoload.php';
     require_once __DIR__.'/../src/RepeatCounter.php';
 
+    $app = new Silex\Application();
+
+
+    $app->get('/', function() {
+
+        return "HELLO";
+
+    });
+
+    return $app;
+
  ?>
