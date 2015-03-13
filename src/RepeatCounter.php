f15aa84a40c0934154f63942462cc9fda0a4623c
Tom Mertz <tom.mertz@verizon.net>
2015-03-13 10:11:32 -0700

    add code to pass single letter test

--- src/RepeatCounter.php
+++ src/RepeatCounter.php
@@ -2,8 +2,17 @@
 
     class RepeatCounter {
 
-        function countRepeats() {
-            
+        function countRepeats($input_word, $string_to_search) {
+            //create a new array of each word from the sentence
+            $list_of_words = explode(" ", $string_to_search);
+
+            //loop through the list of words and find if there's a match
+            foreach($list_of_words as $word)
+            {
+                if($input_word == $word) {
+                    return 1;
+                }
+            }
         }
 
     }
